2536b9589eed225142e4c53baf416936806fa555
Brandon Wharton <whartonbm@gmail.com>
2021-05-17 16:10:57 -0500

    got my image to pop up using an img tag

--- client.js
+++ client.js
@@ -4,4 +4,15 @@ $(readyNow);
 
 function readyNow() {
     console.log('jquery Loaded');
+    appendPersonDivs();
 }
+
+function appendPersonDivs() {
+    // append a div containing profile pictures for everyone on people objects
+    $('#peopleDivs').append(`
+        <div>
+            <img src="http://github.com/brandonwharton.png?size=250" alt="Profile image of Brandon">
+        </div>
+    `);
+} // end appendPersonDivs
+
